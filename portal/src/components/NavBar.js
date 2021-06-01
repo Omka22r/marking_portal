599c80f2cecf426a5500da00772e8d90ca232525
Omkar <osidhu2@bcit.ca>
2021-06-01 14:27:52 -0700

    Creating auth function in backend for user login

--- portal/src/components/NavBar.js
+++ portal/src/components/NavBar.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Navbar, Button, Nav } from 'react-bootstrap';
 import { Link, withRouter } from 'react-router-dom';
+import { BoxArrowLeft, Easel } from 'react-bootstrap-icons';
 
 class NavBar extends Component {
 
@@ -9,41 +10,48 @@ class NavBar extends Component {
 
         this.state = {
 
-          
+
 
         }
     }
 
 
 
-    componentDidMount(){
+    componentDidMount() {
 
-        
+
     }
 
 
     render() {
 
-      
+
         return (
-            
+
             <Navbar bg="dark" variant="dark">
-               
+
                 <Nav className="d-flex flex-column">
-                <Navbar.Brand href="#home">Sam Tucker</Navbar.Brand>
-                    <Navbar.Text>
-                       Instructor
-                    </Navbar.Text>
-                  
+                    <Navbar.Brand className="text-info" href="#home">
+                        <Easel className="m-2" size="42" />
+                    Marking Portal
+                    </Navbar.Brand>
+
+
+
 
                 </Nav>
                 <Navbar.Toggle />
                 <Navbar.Collapse className="justify-content-around d-flex align-items-center">
 
-                    <Nav className="ml-auto">
-                        <Link className="text-dark" to={'/'}>
-                            <Button size="sm" className="m-1">Logout</Button>
-                        </Link>
+                    <Nav className="ml-auto d-flex align-items-center">
+                        <Navbar.Text className="mr-3 text-white">
+                            <h5>Omkar Sidhu</h5>
+                            <p>Instructor</p>
+                            <Link className="text-dark" to={'/'}>
+                                <Button size="sm" >Logout <BoxArrowLeft className="ml-2" size="18" /></Button>
+                            </Link>
+                        </Navbar.Text>
+
                     </Nav>
 
 
--- portal/src/pages/LandingPage.js
+++ portal/src/pages/LandingPage.js
@@ -2,6 +2,7 @@
 import React, { Component } from 'react';
 import { Button, Container, Form, Spinner } from 'react-bootstrap';
 import { Link, withRouter } from 'react-router-dom';
+import { BoxArrowRight, Easel } from 'react-bootstrap-icons';
 
 class LandingPage extends Component {
 
@@ -23,18 +24,58 @@ class LandingPage extends Component {
 
     }
 
+    signIn(e) {
+
+        e.preventDefault();
+        this.setState({ loading: true });
+
+        fetch(`http://localhost:8000/api/users/auth?user=${this.state.username}&pass=${this.state.password}`)
+            .then(response => response.json())
+            .then(data => {
+                if (data.error) {
+                    this.setState({ error: data.error, loading: false })
+                }
+                else {
+                    console.log('Authenticated');
+                    this.setState({ loading: false });
+
+                    // Set LocalLogIn
+
+                    let auth = {
+                        "loggedIn": true,
+                        "userId": data.user._id,
+                        "name": data.user.name,
+                        "usertype": data.user.usertype
+
+                    };
+
+                    localStorage.setItem('local_auth', JSON.stringify(auth));
+
+                    if (data.user.usertype === 'Student') {
+                        this.props.history.push(`/${data.user.usertype}?id=${data.user._id}`);
+                    } else {
+                        this.props.history.push(`/${data.user.usertype}`);
+                    }
+
+                }
+            }).catch(err => {
+                this.setState({ error: JSON.stringify(err), loading: false })
+            });
+
+    }
+
     
     render() {
 
 
         return (
-            <Container style={{ minHeight: '30vh' }} className="bg-light mt-5 p-5 d-flex text-dark flex-column justify-content-around col-lg-4 col-md-8">
-                <h2 className="text-info" style={{ letterSpacing: 2 }}>Marking Portal </h2>
+            <Container style={{ minHeight: '30vh' }} className="bg-light mt-5 p-5 d-flex text-dark flex-column justify-content-center col-lg-4 col-md-8">
+                <h2 className="text-info" style={{ letterSpacing: 2 }}>Marking Portal <Easel className="ml-2" size="45" /></h2>
 
                 {this.state.error !== null ?
                     <h6 className="text-danger mt-3">{this.state.error}</h6> : null
                 }
-                <Form className="mt-2 text-dark">
+                <Form onSubmit={(e) => this.signIn(e)} className="mt-2 text-dark">
                     <Form.Group>
                         <Form.Label><h5>Username</h5></Form.Label>
                         <Form.Control value={this.state.username} onChange={(e) => this.setState({ username: e.target.value })} type="text" placeholder="Username" />
@@ -46,9 +87,9 @@ class LandingPage extends Component {
 
                     {
                         !this.state.loading ?
-                            <Button type="submit" disabled={this.state.username.length === 0 || this.state.password.length === 0}
+                            <Button size="sm" type="submit" disabled={this.state.username.length === 0 || this.state.password.length === 0}
                             variant="primary">
-                                Log In
+                                LOG IN <BoxArrowRight className="ml-2" size="18" />
                         </Button> :
                             <Spinner className="mx-auto" animation="grow" />
                     }
